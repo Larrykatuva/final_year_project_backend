537c02c4ab4a77f1b4f8ff073fe6ee19fa8a8b01
Larrykatuva <larry.katuva@gmail.com>
2021-08-29 13:09:24 +0300

    User controllers activation and reset helpers

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv').config();
 const User = require("../sequelize/models").User;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const createError = require('http-errors');
 
 
 /**
@@ -32,7 +31,7 @@ const registerUser = async(data) => {
             county, 
             password:hashPassword,
             phone, 
-            imageUrl: imageUrl? imageUrl: "",
+            imageUrl: imageUrl,
             role: "user", 
             age:dateOfBirth,
             is_active:false,
@@ -44,7 +43,7 @@ const registerUser = async(data) => {
 /**
  * Login user
  * @param {Object} userData 
- * @returns boolean: if logged in or not
+ * @returns {boolean} if logged in or not
  */
 const loginUser = async(user, password) => {
     const response = user && bcrypt.compareSync(password, user.password);
@@ -69,9 +68,69 @@ const generateAccessToken = async(userId) => {
 
 
 
+/**
+ * Activating user account by Id
+ * @param {number} userId 
+ * @returns {boolean} true is account is activated and false otherwise
+ */
+const activateUserEmail = async(userId) => {
+    return User.update({is_active: true, is_verified: true},{
+        where: {
+            id: userId
+        }
+    })
+}
+
+
+/**
+ * Deactivating user account by Id
+ * @param {number} userId 
+ * @returns {boolean} true is account is deactivated and false otherwise
+ */
+const deactivateUserAccount = async(userId) => {
+    return User.update({is_active: false},{
+        where: {
+            id: userId
+        }
+    })
+}
+
+
+/**
+ * Check user by Id
+ * @param {number} userId 
+ * @returns {object} object of the user matching the email
+ */
+const checkUserById = async (userId) => {
+    return User.findOne({where: {id: userId}})
+}
+
+
+/**
+ * Updating user password matching the passed email
+ * @param {string} email 
+ * @param {string} password 
+ * @returns {object} user details of the updated user
+ */
+const updateUserPassword = async (email, password) => {
+    const salt = await bcrypt.genSaltSync(10);
+    const hashPassword = await bcrypt.hashSync(password, salt);
+    return await User.update({password: hashPassword}, {
+        where: {
+            email,
+        }
+    })
+}
+
+
+
 module.exports = {
     registerUser,
     checkIfUserEmailExists,
     loginUser,
-    generateAccessToken
+    generateAccessToken,
+    activateUserEmail,
+    deactivateUserAccount,
+    checkUserById,
+    updateUserPassword
 }
