d66fc845db44ce9c33861c75ed2a47c353c7920a
Larrykatuva <larry.katuva@gmail.com>
2021-08-29 13:12:58 +0300

    Send password activation link

--- src/services/email.js
+++ src/services/email.js
@@ -31,6 +31,37 @@ const sendActivationMail = async(receipientEmail, ur) => {
     }
 }
 
+
+/**
+ * Function to send reset password link
+ * @param {string} email
+ * @returns {boolean} true or false
+ */
+const sendResetPasswordEmail = async(receipientEmail, ur) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: process.env.EMAIL_SERVICE,
+            auth: {
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+        const info = await transporter.sendMail({
+            from: process.env.EMAIL_ADDRESS,
+            to: receipientEmail,
+            subject: "Account activation",
+            text: "please use the given link to reset your account password",
+            html: `
+                <h2>Please click on given link to reset yout account password</h2>
+                <p>${ur.href}</p>`
+        });
+        return info ? true : false
+    } catch (error) {
+        return false
+    }
+}
+
 module.exports = {
-    sendActivationMail
+    sendActivationMail,
+    sendResetPasswordEmail
 }
